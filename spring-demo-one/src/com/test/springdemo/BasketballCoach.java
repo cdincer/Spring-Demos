0bd62f8dd91b90673c93618236874c1bca603578
Can Dincer <dincercan@gmail.com>
2019-11-14 13:12:43 +0300

    Initial

---
+++ spring-demo-one/src/com/test/springdemo/BasketballCoach.java
@@ -0,0 +1,12 @@
+package com.test.springdemo;
+
+public class BasketballCoach implements Coach
+{
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Spend 10 minutes doing suicide drills";
+	}
+
+}
