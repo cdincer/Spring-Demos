508f11f633d5d8e846c7b8f9b5a04030763991df
cdincer <dincercan@gmail.com>
2019-11-15 03:46:03 +0300

    Constructor

--- spring-demo-one/Classes/com/test/springdemo/CricketCoach.java
+++ spring-demo-one/Classes/com/test/springdemo/CricketCoach.java
@@ -11,7 +11,13 @@ public class CricketCoach implements Coach {
 		System.out.println("No argument CricketCoach argument triggered");
 	}
 	
-	
+	//Setter method for CricketCoach
+	public void setPerAss(PerformanceAssessment perAss) {
+		System.out.println("setPerAss reached");
+		PerAss = perAss;
+	}
+
+
 	@Override
 	public String getDailyWorkout() {
 		return "Practice cricket for 15 minutes";
