4c0359956e88b955f63f36165ec00eb5ef0aca98
Can Dincer <dincercan@gmail.com>
2019-11-21 15:59:27 +0300

    CricketCoach literal values injected(Hardcoded Version)

--- spring-demo-one/Classes/com/test/springdemo/CricketCoach.java
+++ spring-demo-one/Classes/com/test/springdemo/CricketCoach.java
@@ -5,12 +5,35 @@ public class CricketCoach implements Coach {
 	private PerformanceAssessment PerAss;
 	
 	//No-argument constructor
+	private String emailAddress;
+	private String team;
 	
 	public CricketCoach()
 	{
 		System.out.println("No argument CricketCoach argument triggered");
+		
 	}
 	
+	//Email and Team getter setter block begin
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("CricketCoach email assigned :"+ emailAddress);
+		this.emailAddress = emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("CricketCoaches team name assigned :"+team);
+		this.team = team;
+	}
+	//Email and Team getter setter block end
+
 	public CricketCoach(PerformanceAssessment performanceReport) {
 		super();
 		PerAss = performanceReport;
--- spring-demo-one/src/com/test/springdemo/SetterDemoApp.java
+++ spring-demo-one/src/com/test/springdemo/SetterDemoApp.java
@@ -7,11 +7,25 @@ public class SetterDemoApp {
 	public static void main(String[] args) {
 		// load the spring configuration file
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
 		//retrieve bean from spring container
+		CricketCoach theCoach = context.getBean("myCricketCoach",CricketCoach.class);
+		
 		
 		//call methods on the bean
+		System.out.println(theCoach.getDailyWorkout());
+		
+		System.out.println(theCoach.getDailyPerformance());
+		
+		System.out.println("====CricketCoachGetValues====Begin");
+		System.out.println(theCoach.getTeam());
+		System.out.println(theCoach.getEmailAddress());
+		System.out.println("====CricketCoachGetValues====End");
+
+		
 		
 		//close the context
+		context.close();
 	}
 
 }
