4184684dd8b5237c21796d86d348f7483f1f0843
Can Dincer <dincercan@gmail.com>
2019-11-27 14:34:30 +0300

    Settings thing  On Win

---
+++ spring-demo-web/Src/com/test/springdemo/web/HomeController.java
@@ -0,0 +1,16 @@
+package com.test.springdemo.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController
+{
+	@RequestMapping("/")
+	public String showPage()
+	{
+		//Page Menu Name Returner
+	return "main-menu";	
+	}
+
+}
