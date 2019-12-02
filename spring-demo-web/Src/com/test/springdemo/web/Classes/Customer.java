2b1e1ce0a14a00ee882b259378ed9f864376ce6b
Can Dincer <dincercan@gmail.com>
2019-12-02 11:58:46 +0300

    Validation

---
+++ spring-demo-web/Src/com/test/springdemo/web/Classes/Customer.java
@@ -0,0 +1,27 @@
+package com.test.springdemo.web.Classes;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class Customer {
+	
+	@NotNull(message="is required")
+	@Size(min=1)
+	private String firstName;
+	
+	private String lastName;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
--- spring-demo-web/WebContent/WEB-INF/view/main-menu.jsp
+++ spring-demo-web/WebContent/WEB-INF/view/main-menu.jsp
@@ -3,4 +3,6 @@
 <h1>Student Records System</h1>
 <h2>Records Menu</h2>
 <h2><a href="student/showForm">Student Record Adding</a></h2>
+<h2><a href="">Customer Record Adding</a></h2>
+
 </html>
