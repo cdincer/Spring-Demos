f64ae30ddf11970daa47a4a6dab3392c7640b3ed
Can Dincer <dincercan@gmail.com>
2019-12-03 17:36:56 +0300

    ID Generation Value Changes

--- spring-demo-web/Src/com/test/springdemo/Hibernate/Demo/CreateCustomerDemo.java
+++ spring-demo-web/Src/com/test/springdemo/Hibernate/Demo/CreateCustomerDemo.java
@@ -10,20 +10,25 @@ public class CreateCustomerDemo {
 
 	public static void main(String[] args) {
     SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
-							 .addAnnotatedClass(Customer.class)
-							 .buildSessionFactory();
-    
+							  .addAnnotatedClass(Customer.class)
+							  .buildSessionFactory();
+                             
     Session session = factory.getCurrentSession();
     
     try {
     	//use the session object so save Java object
-    	System.out.println("Creating new customer");
-    	Customer tempCustomer= new Customer("Paul","Climber","paul@climber.com");
-    	
+    	System.out.println("Creating 3 new customer");
+    	Customer tempCustomer1= new Customer("Paul","Walker1","paul@walker.com");
+    	Customer tempCustomer2= new Customer("Paul","Crawler2","paul@crawler.com");
+    	Customer tempCustomer3= new Customer("Paul","Flier3","paul@flier.com");
     	session.beginTransaction();
     	
     	System.out.println("Adding the customer");
-    	session.save(tempCustomer);
+    	session.save(tempCustomer1);
+    	session.save(tempCustomer2);
+    	session.save(tempCustomer3);
+    	
+    	
     	session.getTransaction().commit();
     	
     }
