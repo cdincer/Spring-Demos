0869012cc29f512c3c686b94fcac5812f94e0384
Can Dincer <dincercan@gmail.com>
2019-12-03 17:03:30 +0300

    CreateCustomer simply

--- spring-demo-web/Src/com/test/springdemo/Hibernate/Demo/CreateCustomerDemo.java
+++ spring-demo-web/Src/com/test/springdemo/Hibernate/Demo/CreateCustomerDemo.java
@@ -1,10 +1,37 @@
 package com.test.springdemo.Hibernate.Demo;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.test.springdemo.Hibernate.Entity.Customer;
+
 public class CreateCustomerDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+    SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
+							 .addAnnotatedClass(Customer.class)
+							 .buildSessionFactory();
+    
+    Session session = factory.getCurrentSession();
+    
+    try {
+    	//use the session object so save Java object
+    	System.out.println("Creating new customer");
+    	Customer tempCustomer= new Customer("Paul","Climber","paul@climber.com");
+    	
+    	session.beginTransaction();
+    	
+    	System.out.println("Adding the customer");
+    	session.save(tempCustomer);
+    	session.getTransaction().commit();
+    	
+    }
+    finally {
+    	factory.close();
+    }
+    
+    
 	}
 
 }
