8fc9a95e58363175a9fba88284385ec1b9d4bcb4
Can Dincer <dincercan@gmail.com>
2019-12-05 12:15:13 +0300

    Hibernate Cascade OneToOne

--- spring-demo-web/Src/com/test/springdemo/Hibernate/Demo/CreateDemo.java
+++ spring-demo-web/Src/com/test/springdemo/Hibernate/Demo/CreateDemo.java
@@ -5,23 +5,38 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 import com.test.springdemo.Hibernate.Entity.Customer;
+import com.test.springdemo.Hibernate.Entity.CustomerDetail;
 
-public class CreateCustomerDemo {
+public class CreateDemo {
 
 	public static void main(String[] args) {
     SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
 							  .addAnnotatedClass(Customer.class)
+							  .addAnnotatedClass(CustomerDetail.class)
+
 							  .buildSessionFactory();
                              
     Session session = factory.getCurrentSession();
     
     try {
     	//use the session object so save Java object
-    	System.out.println("Creating 3 new customer");
+    	System.out.println("Creating 3 new customers");
     	Customer tempCustomer1= new Customer("Paul","Walker","paul@walker.com");
     	Customer tempCustomer2= new Customer("Paul","Crawler","paul@crawler.com");
     	Customer tempCustomer3= new Customer("Paul","Flier","paul@flier.com");
     	Customer tempCustomer4= new Customer("Clancy","Brown","clancy@brown.com");
+    	
+    	
+    	CustomerDetail tempCustDetail = new CustomerDetail("Walker@fb","Love Walking");
+    	CustomerDetail tempCustDetail2 = new CustomerDetail("Crawler@fb","Love Crawling");
+    	CustomerDetail tempCustDetail3 = new CustomerDetail("Flier@fb","Love Flying");
+    	CustomerDetail tempCustDetail4 = new CustomerDetail("Clancy@fb","Socializing");
+
+    	tempCustomer1.setCustomerDetail(tempCustDetail);
+    	tempCustomer2.setCustomerDetail(tempCustDetail2);
+    	tempCustomer3.setCustomerDetail(tempCustDetail3);
+    	tempCustomer4.setCustomerDetail(tempCustDetail4);
+
     	
     	session.beginTransaction();
     	
--- spring-demo-web/Src/com/test/springdemo/Hibernate/Entity/CustomerDetail.java
+++ spring-demo-web/Src/com/test/springdemo/Hibernate/Entity/CustomerDetail.java
@@ -1,10 +1,12 @@
 package com.test.springdemo.Hibernate.Entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
 import javax.persistence.Table;
 
 @Entity
