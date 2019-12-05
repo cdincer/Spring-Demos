da112d9ddd72b8d807a07147222f37e3f47a7498
Can Dincer <dincercan@gmail.com>
2019-12-05 11:31:30 +0300

    Hibernate One-to-One Prep

--- spring-demo-web/Src/TestJdbc.java
+++ spring-demo-web/Src/TestJdbc.java
@@ -6,7 +6,7 @@ public class TestJdbc {
 
 	public static void main(String[] args) {
 
-		String jdbcUrl = "jdbc:mysql://localhost:3306/hb_customer_tracker?useSSL=false&serverTimezone=UTC";
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?useSSL=false&serverTimezone=UTC";
 		String user="hbcustomer";
 		String pass="12345";
 		
