89c0320d06c3aaa499fe99bde338454f00e2c5de
Can Dincer <dincercan@gmail.com>
2019-12-06 10:07:00 +0300

    OneToMany DB Prep

--- spring-demo-web/Src/TestJdbc.java
+++ spring-demo-web/Src/TestJdbc.java
@@ -6,7 +6,7 @@ public class TestJdbc {
 
 	public static void main(String[] args) {
 
-		String jdbcUrl = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?useSSL=false&serverTimezone=UTC";
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb-03-one-to-many?useSSL=false&serverTimezone=UTC";
 		String user="hbcustomer";
 		String pass="12345";
 		
