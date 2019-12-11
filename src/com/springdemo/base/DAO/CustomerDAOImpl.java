42a8bc3a92a9160e45a8525fc5b2a3234d101475
cdincer <dincercan@gmail.com>
2019-12-11 14:55:22 +0300

    CustomerDAOImp completed

--- src/com/springdemo/base/DAO/CustomerDAOImpl.java
+++ src/com/springdemo/base/DAO/CustomerDAOImpl.java
@@ -4,21 +4,33 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.springdemo.Hibernate.Entity.Customer;
 
+@Repository
 public class CustomerDAOImpl implements CustomerDAO {
 	
 @Autowired
 private SessionFactory sessionFactory;
-	
+	//Transactional Annotation helps with not opening session and closing every time.
 	@Override
 	@Transactional
 	public List<Customer> getCustomers() {
 		
-		return null;
+	Session currentSession= sessionFactory.getCurrentSession();
+	
+	
+	Query<Customer> theQuery=currentSession.createQuery("from Customer", Customer.class);
+	
+	// execute query and get result list.
+	List<Customer> customers = theQuery.getResultList();
+		
+		return customers;
 	}
 
 }
