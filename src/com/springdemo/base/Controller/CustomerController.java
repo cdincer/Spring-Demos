6497f385584c6ce22571d85921a7e5d711a1f556
cdincer <dincercan@gmail.com>
2019-12-11 15:33:52 +0300

    First View with Hibernate

--- src/com/springdemo/base/Controller/CustomerController.java
+++ src/com/springdemo/base/Controller/CustomerController.java
@@ -1,17 +1,34 @@
 package com.springdemo.base.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.springdemo.Hibernate.Entity.Customer;
+import com.springdemo.base.DAO.CustomerDAO;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
-	@RequestMapping("/list")
+	@Autowired
+	private CustomerDAO customerDAO;
 	
+	
+	@RequestMapping("/list")
 		public String listCustomers(Model theModel)
 		{
+		// get customer DAO stuff
+		List<Customer> RetrCustomers = customerDAO.getCustomers();
+		
+		//added to model here
+		
+		
+		theModel.addAttribute("customers",RetrCustomers);
+		
 		return "list-customers";
 		}
 	
