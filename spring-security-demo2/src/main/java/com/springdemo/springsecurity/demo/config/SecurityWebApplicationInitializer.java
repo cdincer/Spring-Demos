817d4f7e83062e96d468ff93738942a690f91d74
Can Dincer <dincercan@gmail.com>
2019-12-16 21:15:16 +0300

    Login-Security Implemented

---
+++ spring-security-demo2/src/main/java/com/springdemo/springsecurity/demo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.springdemo.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
