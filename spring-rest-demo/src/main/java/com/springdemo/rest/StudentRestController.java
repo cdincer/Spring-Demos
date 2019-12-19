52c13aa436544f3e09e153069c0aa411503685be
Can Dincer <dincercan@gmail.com>
2019-12-19 17:34:51 +0300

    Custom/Generic Error Message REST for Single Controller

--- spring-rest-demo/src/main/java/com/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/springdemo/rest/StudentRestController.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -46,8 +49,45 @@ public class StudentRestController {
 	@GetMapping("/students/{studentId}")
 	public Student getStudent(@PathVariable int studentId)
 	{
+	
+		if(studentId>= theStudents.size()|| (studentId<0))
+		{
+			throw new StudentNotFoundException("Student id could not be found "+studentId);
+		}
 		
 		return theStudents.get(studentId);
 	}
+	
+	//Add an exception handler using @ExceptionHandler,for not found student ids.
+	
+	@ExceptionHandler
+	public ResponseEntity <StudentErrorResponse> handleException(StudentNotFoundException exc)
+	{
+			StudentErrorResponse error= new StudentErrorResponse();
+			
+			error.setStatus(HttpStatus.NOT_FOUND.value());
+			error.setMessage(exc.getMessage());
+			error.setTimeStamp(System.currentTimeMillis());
+		
+		return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
+		
+	}
+	
+	
+	///Handle generic exceptions,basically for all.
+	@ExceptionHandler
+	public ResponseEntity <StudentErrorResponse> handleException(Exception exc)
+	{
+		StudentErrorResponse error = new StudentErrorResponse();
+
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		error.setMessage(exc.getMessage()+" modified");
+		error.setTimeStamp(System.currentTimeMillis());
+	
+	   return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
+	}
+	
+	
+	
 	
 }
