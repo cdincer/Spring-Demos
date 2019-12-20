cb7347efe747adac4c37316a23c53cad3e99f673
Can Dincer <dincercan@gmail.com>
2019-12-20 11:29:26 +0300

    Maven Infused Crm project

---
+++ spring-crm-rest/src/main/java/com/springdemocrm/dao/CustomerDAO.java
@@ -0,0 +1,17 @@
+package com.springdemocrm.dao;
+
+import java.util.List;
+
+import com.springdemocrm.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
