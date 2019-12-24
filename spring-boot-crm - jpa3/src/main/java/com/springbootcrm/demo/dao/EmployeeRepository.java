0efca51fe8931adbac23dd7f0c3f0924bd060f81
Can Dincer <dincercan@gmail.com>
2019-12-24 12:30:20 +0300

    Data Rest Configuration and Notes

--- spring-boot-crm - jpa3/src/main/java/com/springbootcrm/demo/dao/EmployeeRepository.java
+++ spring-boot-crm - jpa3/src/main/java/com/springbootcrm/demo/dao/EmployeeRepository.java
@@ -1,9 +1,15 @@
 package com.springbootcrm.demo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.springbootcrm.demo.entity.Employee;
 
+
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 //http://localhost:8080/api-res/employees/4 is for puts,everything else is same
+//we added api-res through application.properties file.	
+//@RepositoryRestResource helps us change the name of endpoints from employees to members
+	
 }
