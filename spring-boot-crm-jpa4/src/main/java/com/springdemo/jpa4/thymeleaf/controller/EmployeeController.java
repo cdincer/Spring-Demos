2112acb875dd3297fd212f3ebcdc1d44157ac5aa
Can Dincer <dincercan@gmail.com>
2019-12-25 15:05:35 +0300

    Listing Employees with DataTable from Thymeleaf

---
+++ spring-boot-crm-jpa4/src/main/java/com/springdemo/jpa4/thymeleaf/controller/EmployeeController.java
@@ -0,0 +1,61 @@
+package com.springdemo.jpa4.thymeleaf.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.springdemo.jpa4.thymeleaf.model.Employee;
+
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+	// load employee data
+	
+	private List<Employee> theEmployees;
+	
+	@PostConstruct
+	private void loadData() {
+		
+		// create employees
+		Employee emp1 = new Employee(1, "Leslie", "Andrews", "leslie@gmail.com");
+		Employee emp2 = new Employee(2, "Emma", "Baumgarten", "emma@gmail.com");
+		Employee emp3 = new Employee(3, "Avani", "Gupta", "avani@gmail.com");
+
+		// create the list
+		theEmployees = new ArrayList<>();
+		
+		// add to the list
+		theEmployees.add(emp1);
+		theEmployees.add(emp2);
+		theEmployees.add(emp3);
+	
+	}
+	
+	// add mapping for "/list"
+
+	@GetMapping("/list")
+	public String listEmployees(Model theModel) {
+		
+		// add to the spring model
+		theModel.addAttribute("employees", theEmployees);
+		
+		return "list-employees";
+	}
+}
+
+
+
+
+
+
+
+
+
